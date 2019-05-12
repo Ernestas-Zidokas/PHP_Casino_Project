07cc9e493ae344de10b18a20ed00257e0742957a
Katamaranas <ernestas.zidokas@gmail.com>
2019-05-12 19:29:08 +0300

    added balance counting with database

--- app/classes/controller/BlackJack.php
+++ app/classes/controller/BlackJack.php
@@ -9,6 +9,13 @@ class BlackJack extends Base {
     protected $player_repo;
     protected $blackjack;
     protected $game_repo;
+    protected $user_repo;
+    protected $user;
+    protected $balance;
+    protected $form_bet;
+    protected $form_play;
+    protected $player_input_bet;
+    protected $player_input_play;
 
     public function __construct() {
         if (!\App\App::$session->isLoggedIn() === true) {
@@ -20,6 +27,11 @@ class BlackJack extends Base {
         $this->player_repo = new \App\Objects\BlackJack\Player\Repository(\App\App::$db_conn);
         $this->player = $this->player_repo->load(\App\App::$session->getUser()->getEmail());
         $this->game_repo = new \App\Objects\BlackJack\Game\Repository(\App\App::$db_conn);
+        $this->user_repo = new \App\User\Repository(\App\App::$db_conn);
+        $this->user = $this->user_repo->load(\App\App::$session->getUser()->getEmail());
+        $this->balance = $this->user->getBalance();
+        $this->form_bet = new \App\Objects\Form\BlackJackBet();
+        $this->form_play = new \App\Objects\Form\BlackJackPlay();
 
         if (!$this->player) {
             $this->player = new \App\Objects\BlackJack\Player\Player([
@@ -31,8 +43,7 @@ class BlackJack extends Base {
         }
 
         if (!$this->player->getInGame()) {
-            $form_bet = new \App\Objects\Form\BlackJackBet();
-            $status_bet = $form_bet->process();
+            $status_bet = $this->form_bet->process();
 
             $view = new \Core\Page\View([
                 'title' => 'Place your bet'
@@ -41,22 +52,23 @@ class BlackJack extends Base {
 
             switch ($status_bet) {
                 case \App\Objects\Form\BlackJackBet::STATUS_SUCCESS:
+                    $this->player_input_bet = $this->form_bet->getInput();
+
                     $deck = new \App\Objects\BlackJack\Game\Deck();
                     $blackjack = new \App\Objects\BlackJack\BlackJack();
                     $blackjack->setDeck($deck->create());
                     $blackjack->save();
 
-                    $player_input_bet = $form_bet->getInput();
                     $this->player = new \App\Objects\BlackJack\Player\Player([
                         'email' => \App\App::$session->getUser()->getEmail(),
                         'in_game' => 1,
-                        'bet_size' => $player_input_bet['bet']
+                        'bet_size' => $this->player_input_bet['bet']
                     ]);
                     $this->player_repo->update($this->player);
                     break;
             }
 
-            $this->page['content'] .= $form_bet->render();
+            $this->page['content'] .= $this->form_bet->render();
         }
 
         if ($this->player->getInGame()) {
@@ -64,9 +76,7 @@ class BlackJack extends Base {
                 'title' => 'This is your night!'
             ]);
             $this->page['content'] = $view->render(ROOT_DIR . '/app/views/content.tpl.php');
-
-            $form_play = new \App\Objects\Form\BlackJackPlay();
-            $status_play = $form_play->process();
+            $status_play = $this->form_play->process();
 
             $blackjack = new \App\Objects\BlackJack\BlackJack();
             $blackjack->setDeck($this->game_repo->loadAll());
@@ -75,18 +85,34 @@ class BlackJack extends Base {
 
             switch ($status_play) {
                 case \App\Objects\Form\BlackJackPlay::STATUS_SUCCESS:
-                    $player_input_play = $form_play->getInput();
+                    $player_input_play = $this->form_play->getInput();
+
                     if ($player_input_play['action'] == 'hit') {
                         $blackjack->hit();
                     }
                     if ($player_input_play['action'] == 'stand') {
                         $stand = $blackjack->stand();
                         if ($stand == $blackjack::OUTCOME_WIN) {
-                            $this->page['content'] = 'You win!';
+                            $view = new \Core\Page\View([
+                                'status' => 'You won!',
+                                'amount' => 'Win amount: ' . $this->winAmount()
+                            ]);
+
+                            $this->page['content'] .= $view->render(ROOT_DIR . '/app/views/blackJack.tpl.php');
                         } elseif ($stand == $blackjack::OUTCOME_TIE) {
-                            $this->page['content'] = 'Its a tie!';
+                            $view = new \Core\Page\View([
+                                'status' => 'Its a tie!',
+                            ]);
+
+                            $this->page['content'] .= $view->render(ROOT_DIR . '/app/views/blackJack.tpl.php');
                         } else {
-                            $this->page['content'] = 'Try again!';
+
+                            $view = new \Core\Page\View([
+                                'status' => 'Try again!',
+                                'amount' => 'Lose amount: ' . $this->loseAmount()
+                            ]);
+
+                            $this->page['content'] .= $view->render(ROOT_DIR . '/app/views/blackJack.tpl.php');
                         }
                     }
                     if ($player_input_play['action'] == 'play_again') {
@@ -95,6 +121,7 @@ class BlackJack extends Base {
                             'in_game' => 0,
                             'bet_size' => 0
                         ]);
+
                         $this->game_repo->deleteAll();
                         $this->player_repo->update($this->player);
                     }
@@ -105,13 +132,38 @@ class BlackJack extends Base {
             $cards = new \Core\Page\View([
                 'title' => 'Blackjack',
                 'dealer' => $blackjack->getDealersCards(),
-                'your' => $blackjack->getPlayerCards()
+                'player' => $blackjack->getPlayerCards()
             ]);
 
             $this->page['content'] .= $cards->render(ROOT_DIR . '/app/views/blackJack.tpl.php');
 
-            $this->page['content'] .= $form_play->render();
+            $this->page['content'] .= $this->form_play->render();
         }
+    }
+
+    public function winAmount() {
+        $balance = ($this->balance - $this->player->getBetSize()) + $this->player->getBetSize() * 2;
+
+        $users_balance = new \App\User\User([
+            'email' => \App\App::$session->getUser()->getEmail(),
+            'balance' => $balance
+        ]);
+
+        $this->user_repo->update($users_balance);
+        return $this->player->getBetSize() * 2;
+    }
+
+    public function loseAmount() {
+        $balance = $this->balance - $this->player->getBetSize();
+
+        $users_balance = new \App\User\User([
+            'email' => \App\App::$session->getUser()->getEmail(),
+            'balance' => $balance
+        ]);
+
+        $this->user_repo->update($users_balance);
+
+        return $this->player->getBetSize();
     }
 
 }
--- app/views/blackJack.tpl.php
+++ app/views/blackJack.tpl.php
@@ -1,16 +1,18 @@
 <div class="content">
-    <h1><?php print $view['title']; ?> </h1>
+    <h1><?php isset($view['title']) ? print $view['title'] : '' ?></h1>
+    <h2><?php isset($view['status']) ? print $view['status'] : '' ?></h2>
+    <h3><?php isset($view['amount']) ? print $view['amount'] : '' ?></h3>
     <div>
-        <h2>Dealers hand</h2>
-        <?php foreach ($view['dealer'] as $card): ?>
+        <h3><?php isset($view['dealer']) ? print 'Dealers hand' : '' ?> </h3>
+        <?php foreach (isset($view['dealer']) ? $view['dealer'] : [] as $card): ?>
             <span>
                 <img class="card" src="/images/cards/<?php print $card->getNumber() . $card->getSuit(); ?>.jpg">
             </span>
         <?php endforeach; ?>
     </div>
     <div>
-        <h2>Your hand</h2>
-        <?php foreach ($view['your'] as $card): ?>
+        <h3><?php isset($view['player']) ? print 'Your hand' : '' ?> </h3>
+        <?php foreach (isset($view['player']) ? $view['player'] : [] as $card): ?>
             <span>
                 <img class="card" src="/images/cards/<?php print $card->getNumber() . $card->getSuit(); ?>.jpg">
             </span>
